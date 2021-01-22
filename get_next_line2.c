4e9996bd3b0762005511a926ad4677403dc46500
Timothee GERBERON <tigerber@e3r6p18.42.fr>
2021-01-22 13:22:13 +0100

    1322

--- get_next_line2.c
+++ get_next_line2.c
@@ -6,7 +6,7 @@
 /*   By: tigerber <tigerber@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/21 13:21:55 by tigerber          #+#    #+#             */
-/*   Updated: 2021/01/21 16:02:40 by tigerber         ###   ########.fr       */
+/*   Updated: 2021/01/22 13:13:41 by tigerber         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -60,58 +60,113 @@ char	*ft_strjoin(const char *s1, const char *s2)
 	return (str);
 }
 
-char		*newbuff(char *buff)
+char		*newrest(char *str)
 {
 	int i;
 
 	i = 0;
-	while (buff[i])
+	while (str[i])
 	{
-		if (buff[i] == '\n')
+		if (str[i] == '\n')
 		{
-			return (&buff[i + 1]);
+			return (&str[i + 1]);
 		}
 		i++;
 	}
-	return (buff);
+	return (str);
 }
 
-char		*get_first_line(int fd, char *buff)
+int		ft_tempavbs(char *temp)
 {
-	int ret;
-	char *str;
 	int i;
 
-	ret = 0;
-	str = "";
 	i = 0;
+	while (temp[i])
+	{
+		if (temp[i] == '\n')
+		{
+			return (1);
+		}
+		i++;
+	}
+	return (0);
+}
+
+char		*get_first_line(int fd, char *buff,char **line,char *temp)
+{
+	printf("GFL\n");
+	int ret;
+	int i;
+	char *str;
+
+	str = "";
+	ret = 0;
+	i = 0;
+	if (temp != NULL)
+	{
+		str = ft_strjoin(str, temp);
+	}
 	while ((ret = read(fd, buff, BUFF)))
 	{
+		if (ret == 0)
+		{
+			printf("ret = [%d]\n", ret);
+			return (0);
+		}
 		str = ft_strjoin(str, buff);
 		while (buff[i] && i < ret)
 		{
 			if (buff[i] == '\n')
 			{
-				buff = newbuff(buff);
-				printf("newbuff = [%s]\n", buff);
-				return (str);
+				*line = str;
+				buff = newrest(buff);
+				return (buff);
 			}
 			i++;
 		}
 		i = 0;
 	}
-	return (str);
+	return (buff);
+}
+
+char		*get_temp_line(char *temp, char **line)
+{
+	printf("GTL\n");
+	int i;
+	char *str;
+
+	i = 0;
+	str = "";
+	str = ft_strjoin(str, temp);
+	*line = str;
+	temp = newrest(temp);
+	return (temp);
 }
 
 int		get_next_line(int fd, char **line)
 {
 	static char buff[BUFF + 1];
+	static char *temp;
 
 	buff[BUFF] = '\0';
-	*line = get_first_line(fd, buff);
-	printf("buff de get = [%s]\n", buff);
-	printf("line  = [%s]\n", *line);
-	printf("============================================================\n");
+	if (temp == NULL)
+	{
+		temp = get_first_line(fd, buff, line, temp);
+		printf ("temp GFL = [%s]\n", temp);
+		return (1);
+	}
+	if ((temp != NULL) && (ft_tempavbs(temp) == 0))
+	{
+		temp = get_first_line(fd, buff, line, temp);
+		printf ("temp GFL = [%s]\n", temp);
+		return (1);
+	}
+	if (temp != NULL && ft_tempavbs(temp))
+	{
+		temp = get_temp_line(temp, line);
+		printf ("temp GTL = [%s]\n", temp);
+		return (1);
+	}
 	return (0);
 }
 int		main()
@@ -122,7 +177,22 @@ int		main()
 
 	line = &str;
 	fd = open("text4.txt", O_RDONLY);
-	get_next_line(fd, line);
+	printf("============================================================\n");
+	printf("getnextline 1 = %d\n", get_next_line(fd, line));
+	printf("line = [%s]\n", *line);
+	printf("============================================================\n");
+	printf("getnextline 2  = %d\n", get_next_line(fd, line));
+	printf("line = [%s]\n", *line);
+	printf("============================================================\n");
+	printf("getnextline 3 = %d\n", get_next_line(fd, line));
+	printf("line = [%s]\n", *line);
+	printf("============================================================\n");
+	printf("getnextline 4 = %d\n", get_next_line(fd, line));
+	printf("line = [%s]\n", *line);
+	printf("============================================================\n");
+	printf("getnextline 5 = %d\n", get_next_line(fd, line));
+	printf("line = [%s]\n", *line);
+	printf("============================================================\n");
 
 	return (0);
 }
